1c174f4b88f24a6ab5f87c568cfe55c58224e60f
akc2531 <117503109+akc2531@users.noreply.github.com>
2023-10-05 23:52:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed= new Typed("#element",{
+    strings:["Blogging Website"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+document.getElementById("my1").addEventListener('click',function(){
+window.location.href="https://docs.google.com/document/d/1ii4sY8JH_AQGqfKL355aoCmsBV02PRnK/edit?usp=sharing&ouid=106896646268080985480&rtpof=true&sd=true";
+});
+document.getElementById("my2").addEventListener('click',function()
+{
+window.location.href="https://docs.google.com/document/d/1ii4sY8JH_AQGqfKL355aoCmsBV02PRnK/edit?usp=sharing&ouid=106896646268080985480&rtpof=true&sd=true";
+});
